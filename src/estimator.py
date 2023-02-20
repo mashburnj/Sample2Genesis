75f4a34b5c99fb33a0ccc3408fd1dca5bdb9de10
mashburnj <mashburn.jacob@gmail.com>
2023-02-19 22:29:04 -0600

    update estimator.py to handle all 8 algorithms

---
+++ src/estimator.py
@@ -0,0 +1,62 @@
+import numpy as np
+import os
+import pandas as pd
+import pickle as pk
+from scipy import signal
+from scipy.fft import fft
+from scipy.io import wavfile
+from sklearn.decomposition import PCA
+from tensorflow.keras.models import Sequential, model_from_json
+from tensorflow.keras.layers import Dense
+from convert_to_y12 import convert_to_y12
+
+def estimator(SampleName: str, algorithm: int):
+    # Load sample
+    SampleRate, Audiodata = wavfile.read(SampleName)
+
+    Audiodata = Audiodata[0:141120] # Want to exactly match the length of the WAVs used to train.
+
+    # Run spectral analysis
+    frequencies, times, spectrogram = signal.spectrogram(Audiodata, SampleRate)
+    del frequencies
+    del times
+    print(np.shape(spectrogram))
+
+    SampleFeatures = np.append(spectrogram.flatten(), Audiodata).reshape(1,-1)
+
+    # Load parameters for scaling data.
+    os.chdir('..')
+    os.chdir('./models/')
+    mean = np.loadtxt('mean'+ str(algorithm) + '.csv', delimiter = ',')
+    stdev = np.loadtxt('scale' + str(algorithm) + '.csv', delimiter = ',')
+    # Z = (x - m)/s
+    SampleFeatures = (SampleFeatures - mean)/stdev
+    del mean
+    del stdev
+
+    # Load PCA model.
+    pca = pk.load(open('pca'+ str(algorithm) + '.pkl','rb'))
+    print("Loaded PCA from disk")
+    ReducedFeatures = pca.transform(SampleFeatures)
+    del SampleFeatures
+    del pca
+    del spectrogram
+
+    # Load NN model.
+    json_file = open('model'+ str(algorithm) + '.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    model = model_from_json(loaded_model_json)
+    # load weights into new model
+    model.load_weights("model6.h5")
+    print("Loaded model from disk")
+    
+    #model.compile(loss='mean_square_error', optimizer='adam', metrics=['mean_square_error'])
+
+    predictions = model.predict(ReducedFeatures)
+
+    predictions[0][3] -= 4 # To reverse the operation done to the training set.
+    predictions[0][12] -= 4 # Operation was done to make DT >= 0 due to using ReLU.
+    predictions[0][21] -= 4
+    predictions[0][30] -= 4
+    convert_to_y12(algorithm, predictions[0].round(), SampleName)
--- src/s2g_main.py
+++ src/s2g_main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import os
-from estimator7 import estimator7
+from estimator import estimator
 """Instructions:
 There are 8 booleans, one for each algorithm model. If there is an algorithm you do not
 wish to use, change it to False. Put all WAV samples you wish to make predicted patches
@@ -11,15 +11,16 @@ trained on fixed length WAV files consisting of sounds played for 4 beats in the
 Deflemask tracker under the default tempo.) Use Audacity if you need to resize or convert.
 When the predictors are finished, you will find the patch files in the output directory.
 """
-
-use_algorithm_0 = False
-use_algorithm_1 = False
-use_algorithm_2 = False
-use_algorithm_3 = False
-use_algorithm_4 = False
-use_algorithm_5 = False
-use_algorithm_6 = False
-use_algorithm_7 = True
+use_algorithm = [False] * 8
+# These are indexed according to their corresponding algorithms!
+use_algorithm[0] = False
+use_algorithm[1] = False
+use_algorithm[2] = False
+use_algorithm[3] = False
+use_algorithm[4] = False
+use_algorithm[5] = False
+use_algorithm[6] = False
+use_algorithm[7] = True
 
 # Open directory with samples in it.
 os.chdir('..')
@@ -30,19 +31,8 @@ SampleList = np.array(os.listdir())
 SampleList = SampleList[ np.char.endswith(SampleList, '.wav') ] #ignore everything that's not a .wav file.
 
 for SampleName in SampleList:
-    if use_algorithm_0:
-        print('Algorithm 0 model not implemented and/or tested yet.')
-    if use_algorithm_1:
-        print('Algorithm 1 model not implemented and/or tested yet.')
-    if use_algorithm_2:
-        print('Algorithm 2 model not implemented and/or tested yet.')
-    if use_algorithm_3:
-        print('Algorithm 3 model not implemented and/or tested yet.')
-    if use_algorithm_4:
-        print('Algorithm 4 model not implemented and/or tested yet.')
-    if use_algorithm_5:
-        print('Algorithm 5 model not implemented and/or tested yet.')
-    if use_algorithm_6:
-        print('Algorithm 6 model not implemented and/or tested yet.')
-    if use_algorithm_7:
-        estimator7(SampleName)
+    for i in range(8):
+        if use_algorithm[i] and (i != 7):
+            print('Algorithm ' + str(i) +' model not implemented and/or tested yet.')
+        if use_algorithm[i] and (i == 7):
+            estimator(SampleName, i)
