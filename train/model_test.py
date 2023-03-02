9e3650065d48ec5a185cbe4137ae0533fa6df408
mashburnj <mashburn.jacob@gmail.com>
2023-03-02 06:51:18 -0600

    add trial count output to CLI to track progress

--- train/model_test.py
+++ train/model_test.py
@@ -15,9 +15,9 @@ from model7_train import model7_prep, model7_train
 
 test_algorithm = [False] * 8
 # These are indexed according to their corresponding algorithms!
-test_algorithm[0] = True
-test_algorithm[1] = True
-test_algorithm[2] = True
+test_algorithm[0] = False
+test_algorithm[1] = False
+test_algorithm[2] = False
 test_algorithm[3] = True
 test_algorithm[4] = False # I'll need to figure out a way to test this without running out of GPU RAM.
 test_algorithm[5] = True
@@ -26,88 +26,113 @@ test_algorithm[7] = True
 
 number_of_tests = 30
 
+trial_count = 1
+
 if test_algorithm[0]:
     results = pd.DataFrame(columns = ['loss','mean_square_error', 'mean_absolute_error'])
     RF, RT, VF, VT = model0_prep(False)
     for i in np.arange(0,number_of_tests):
         results.loc[i] = model0_train(save_to_disk = False, TrainFeatures = RF, TrainTargets = RT, ValFeatures = VF, ValTargets = VT)
+        print('Trial ', trial_count, ' complete.')
+        trial_count += 1
     results.loc['Mean'] = [np.mean(results['loss']), np.mean(results['mean_square_error']), np.mean(results['mean_absolute_error'])]
     results.loc['Standard Deviation'] = [np.std(results['loss']), np.std(results['mean_square_error']), np.std(results['mean_absolute_error'])]
     os.chdir('..')
     os.chdir('./metrics/')
     results.to_csv('TestResults0.csv')
+    trial_count = 1
 
 if test_algorithm[1]:
     results = pd.DataFrame(columns = ['loss','mean_square_error', 'mean_absolute_error'])
     RF, RT, VF, VT = model1_prep(False)
     for i in np.arange(0,number_of_tests):
         results.loc[i] = model1_train(save_to_disk = False, TrainFeatures = RF, TrainTargets = RT, ValFeatures = VF, ValTargets = VT)
+        print('Trial ', trial_count, ' complete.')
+        trial_count += 1
     results.loc['Mean'] = [np.mean(results['loss']), np.mean(results['mean_square_error']), np.mean(results['mean_absolute_error'])]
     results.loc['Standard Deviation'] = [np.std(results['loss']), np.std(results['mean_square_error']), np.std(results['mean_absolute_error'])]
     os.chdir('..')
     os.chdir('./metrics/')
     results.to_csv('TestResults1.csv')
+    trial_count = 1
 
 if test_algorithm[2]:
     results = pd.DataFrame(columns = ['loss','mean_square_error', 'mean_absolute_error'])
     RF, RT, VF, VT = model2_prep(False)
     for i in np.arange(0,number_of_tests):
         results.loc[i] = model2_train(save_to_disk = False, TrainFeatures = RF, TrainTargets = RT, ValFeatures = VF, ValTargets = VT)
+        print('Trial ', trial_count, ' complete.')
+        trial_count += 1
     results.loc['Mean'] = [np.mean(results['loss']), np.mean(results['mean_square_error']), np.mean(results['mean_absolute_error'])]
     results.loc['Standard Deviation'] = [np.std(results['loss']), np.std(results['mean_square_error']), np.std(results['mean_absolute_error'])]
     os.chdir('..')
     os.chdir('./metrics/')
     results.to_csv('TestResults2.csv')
+    trial_count = 1
 
 if test_algorithm[3]:
     results = pd.DataFrame(columns = ['loss','mean_square_error', 'mean_absolute_error'])
     RF, RT, VF, VT = model3_prep(False)
     for i in np.arange(0,number_of_tests):
         results.loc[i] = model3_train(save_to_disk = False, TrainFeatures = RF, TrainTargets = RT, ValFeatures = VF, ValTargets = VT)
+        print('Trial ', trial_count, ' complete.')
+        trial_count += 1
     results.loc['Mean'] = [np.mean(results['loss']), np.mean(results['mean_square_error']), np.mean(results['mean_absolute_error'])]
     results.loc['Standard Deviation'] = [np.std(results['loss']), np.std(results['mean_square_error']), np.std(results['mean_absolute_error'])]
     os.chdir('..')
     os.chdir('./metrics/')
     results.to_csv('TestResults3.csv')
+    trial_count = 1
 
 if test_algorithm[4]:
     results = pd.DataFrame(columns = ['loss','mean_square_error', 'mean_absolute_error'])
     RF, RT, VF, VT = model4_prep(False)
     for i in np.arange(0,number_of_tests):
         results.loc[i] = model4_train(save_to_disk = False, TrainFeatures = RF, TrainTargets = RT, ValFeatures = VF, ValTargets = VT)
+        print('Trial ', trial_count, ' complete.')
+        trial_count += 1
     results.loc['Mean'] = [np.mean(results['loss']), np.mean(results['mean_square_error']), np.mean(results['mean_absolute_error'])]
     results.loc['Standard Deviation'] = [np.std(results['loss']), np.std(results['mean_square_error']), np.std(results['mean_absolute_error'])]
     os.chdir('..')
     os.chdir('./metrics/')
     results.to_csv('TestResults4.csv')
+    trial_count = 1
 
 if test_algorithm[5]:
     results = pd.DataFrame(columns = ['loss','mean_square_error', 'mean_absolute_error'])
     RF, RT, VF, VT = model5_prep(False)
     for i in np.arange(0,number_of_tests):
         results.loc[i] = model5_train(save_to_disk = False, TrainFeatures = RF, TrainTargets = RT, ValFeatures = VF, ValTargets = VT)
+        print('Trial ', trial_count, ' complete.')
+        trial_count += 1
     results.loc['Mean'] = [np.mean(results['loss']), np.mean(results['mean_square_error']), np.mean(results['mean_absolute_error'])]
     results.loc['Standard Deviation'] = [np.std(results['loss']), np.std(results['mean_square_error']), np.std(results['mean_absolute_error'])]
     os.chdir('..')
     os.chdir('./metrics/')
     results.to_csv('TestResults5.csv')
+    trial_count = 1
 
 if test_algorithm[6]:
     results = pd.DataFrame(columns = ['loss','mean_square_error', 'mean_absolute_error'])
     RF, RT, VF, VT = model6_prep(False)
     for i in np.arange(0,number_of_tests):
         results.loc[i] = model6_train(save_to_disk = False, TrainFeatures = RF, TrainTargets = RT, ValFeatures = VF, ValTargets = VT)
+        print('Trial ', trial_count, ' complete.')
+        trial_count += 1
     results.loc['Mean'] = [np.mean(results['loss']), np.mean(results['mean_square_error']), np.mean(results['mean_absolute_error'])]
     results.loc['Standard Deviation'] = [np.std(results['loss']), np.std(results['mean_square_error']), np.std(results['mean_absolute_error'])]
     os.chdir('..')
     os.chdir('./metrics/')
     results.to_csv('TestResults6.csv')
+    trial_count = 1
 
 if test_algorithm[7]:
     results = pd.DataFrame(columns = ['loss','mean_square_error', 'mean_absolute_error'])
     RF, RT, VF, VT = model7_prep(False)
     for i in np.arange(0,number_of_tests):
         results.loc[i] = model7_train(save_to_disk = False, TrainFeatures = RF, TrainTargets = RT, ValFeatures = VF, ValTargets = VT)
+        print('Trial ', trial_count, ' complete.')
+        trial_count += 1
     results.loc['Mean'] = [np.mean(results['loss']), np.mean(results['mean_square_error']), np.mean(results['mean_absolute_error'])]
     results.loc['Standard Deviation'] = [np.std(results['loss']), np.std(results['mean_square_error']), np.std(results['mean_absolute_error'])]
     os.chdir('..')
